636caede9b95178fd6847cfa04b8a56450316a36
Ethan Farrell <ethan.farrell.1997@gmail.com>
2022-01-31 09:11:23 -0400

    Docker DB Fix

--- Models/Connection.cs
+++ Models/Connection.cs
@@ -1,4 +1,4 @@
 // database connectivity
 public class Connection {
-    public const string CONNECTION_STRING = "Server=mysql;Database=DBlaunchPad;Uid=Trailblazer780;Pwd=1337;port=3306;SslMode=none;";    
+    public const string CONNECTION_STRING = "Server=mysql;Database=NSCCACC;Uid=Trailblazer780;Pwd=1337;port=3306;SslMode=none;";    
 }
