52591139d537730ceabc54b8905c27d9abe06b61
Ethan Farrell <ethan.farrell.1997@gmail.com>
2022-02-01 11:23:10 -0400

    Auto Logout Added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function startAutoLogout() {
+    // setup timeout that will occur when session has expired (20 minutes = 60000 * 20 milliseconds)
+    window.setTimeout(() => document.location = "/", 1200000);
+    // setup timeout that will occur when session about to expire to warn user
+    window.setTimeout(() => document.getElementById("lblExpire").innerHTML = "WARNING : Session is about to expire!", 1080000);
+}
