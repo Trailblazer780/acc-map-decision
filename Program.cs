ca5d2555cc85beac81876603ebabee96036a2d1d
Tolga Mungan <tmungann@gmail.com>
2022-02-10 12:36:59 -0400

    creates CSV file on startup

--- Program.cs
+++ Program.cs
@@ -6,13 +6,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
 
 namespace accmapdecision
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) {
+            WriteCSV();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -22,5 +25,21 @@ namespace accmapdecision
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+            public static void WriteCSV() {
+                var csvPath = Path.Combine(Environment.CurrentDirectory, $"courses-{DateTime.Now.ToFileTime()}.csv");
+                
+                using (var streamWriter = new StreamWriter(csvPath)) {
+                using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture)) { 
+                    // Change this to database or connect GetCourses() to database
+                    var courses = CourseInfo.GetCourses();
+                    csvWriter.WriteRecords(courses);
+                }
+            }
+            Console.WriteLine("CSV File Created");
+            }
+
+
     }
 }
