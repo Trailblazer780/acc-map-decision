227ea0f9800ffa4668d6ad4b2f0b4c834f7c7538
Ethan Farrell <ethan.farrell.1997@gmail.com>
2022-02-15 12:08:17 -0400

    Fix

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -75,6 +75,25 @@ namespace accmapdecision.Controllers {
             return View("EditCourse", course);
         }
 
+        public IActionResult EditCourseSubmit(int id, string code, string name, string description, string rationale) {
+            // construction of the model
+            Admin = new AdminModel(HttpContext);
+            // if not logged in send user back to home page
+            if (HttpContext.Session.GetString("auth") != "true"){
+                return RedirectToAction("Index", "Home");
+            }
+
+            Course course = new Course();
+            course.id = id;
+            course.course_code = code;
+            course.course_name = name;
+            course.course_description = description;
+            course.course_rationale = rationale;
+
+            return View("AllCourses", Admin);
+        }
+
+
         [HttpPost]
         public IActionResult DeleteCourse(int id, string code, string name, string description, string rationale) {
             Admin = new AdminModel(HttpContext);
