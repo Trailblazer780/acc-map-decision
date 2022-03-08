df1e9d6902806df59895e595ee314f143ad0a3af
Tolga Mungan <tmungann@gmail.com>
2022-03-08 13:32:49 -0400

    added custom headers

--- Models/CSVModel.cs
+++ Models/CSVModel.cs
@@ -4,27 +4,48 @@ using Microsoft.EntityFrameworkCore;
 using System.IO;
 using System.Globalization;
 using CsvHelper;
+using CsvHelper.Configuration;
+using System.Linq;
+using System.Text;
 
 namespace accmapdecision.Models {
 
     public class CSVModel : DbContext {
 
-        public CSVModel() {
-        }
+        // public CSVModel() {
+        // }
+
         // public void WriteCSV(UserResponse userResponse) {
         public void WriteCSV(List<Course> courses) {
 
             var csvPath = Path.Combine(Environment.CurrentDirectory , $"courses-{DateTime.Now.ToFileTime()}.csv");
-            using (var streamWriter = new StreamWriter(csvPath)) {
-            using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture)) { 
-                // var userResponse = UserResponseManager;
-                // csvWriter.WriteRecords(userResponse.questionsAndResponses);
-                csvWriter.WriteRecords(courses);
+            var sb = new StringBuilder();
+            using (var streamWriter = new StringWriter(sb))
+            using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+            {
+                csvWriter.WriteField("Course Code");
+                csvWriter.WriteField("Course Name");
+                csvWriter.WriteField("Description");
+                // csvWriter.WriteField("CourseRationale");
+                csvWriter.NextRecord();
+                foreach (var course in courses)
+                {
+                    csvWriter.WriteField(course.course_code);
+                    csvWriter.WriteField(course.course_name);
+                    csvWriter.WriteField(course.course_description);
+                    // csvWriter.WriteField(course.course_rationale);
+                    csvWriter.NextRecord();
+                }
+                File.WriteAllText(csvPath, sb.ToString(), Encoding.Default);                
                 // Exports all the courses in allCourses for now
-                Console.WriteLine(csvPath); 
+                // Console.WriteLine(csvPath); 
+                csvWriter.Flush();
             }
-        } Console.WriteLine("CSV File Created");
+        Console.WriteLine("CSV File Created");
+        } 
+        // return csvPath;
         }
 
+
     }
-}
+
