e1be207324d5b870ad602b482a1c8ceb9a3dd303
Tolga Mungan <tmungann@gmail.com>
2022-04-14 11:24:57 -0300

    csv new design

--- Models/CSVModel.cs
+++ Models/CSVModel.cs
@@ -13,11 +13,11 @@ namespace accmapdecision.Models {
     public class CSVModel : DbContext {
         public string FileName { get; set; }
         public string csvPath { get; set; }
+
         //-- CSV-UserResponse --//
         public async void WriteCSV(UserResponse userResponse ) { 
             FileName = $"csv-{DateTime.Now.ToFileTime()}.csv";
             var csvPath = Path.Combine(Environment.CurrentDirectory+ "/wwwroot/Files/" , FileName);
-            // var csvPath = Path.Combine(this.Environment.WebRootPath, "Files/", FileName);
             var sb = new StringBuilder();
             using (var streamWriter = new StringWriter(sb))
             using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
@@ -31,26 +31,27 @@ namespace accmapdecision.Models {
                     csvWriter.WriteField(question.selectedOptionText);
                     csvWriter.NextRecord();
                 } csvWriter.NextRecord();
-                int longestSemesterCourses = userResponse.programCourseMap.semesterList.Max(w => w.coursesSelected.Count);
+
+
                 //-- Semester-Courses --//
-                foreach (var semester in userResponse.programCourseMap.semesterList ) {
+                 foreach (var semester in userResponse.programCourseMap.semesterList ) {
                     csvWriter.WriteField(semester.semesterTitle );
-                } csvWriter.NextRecord();
-                int i=0;
-                for (int n=0; n<longestSemesterCourses; n++) {
-                    foreach (var semester in userResponse.programCourseMap.semesterList ) {
-                        if( semester.coursesSelected.Count < i+1)  {
-                            csvWriter.WriteField("");
-                        } else { csvWriter.WriteField(semester.coursesSelected[i].courseName); }
-                    } i++;
+                    csvWriter.NextRecord();
+
+                    foreach (var course in semester.coursesSelected ) {
+                        csvWriter.WriteField(course.courseName );
+                        csvWriter.NextRecord();
+                    }
+                 csvWriter.NextRecord();
+                 }                    
+                //-- Contact Information --//
+                csvWriter.WriteField("\n");
                 csvWriter.NextRecord();
-                } csvWriter.NextRecord();
+
                 File.WriteAllText(csvPath, sb.ToString(), Encoding.Default);    
                 Console.WriteLine(csvPath);        
                 Console.WriteLine("File Name: "+FileName);    
-                //   Exports all the courses in allCourses for now
-                //   Console.WriteLine(csvPath.ToString());
-                //   File.Open(csvPath));
+
                   csvWriter.Flush();
             } Console.WriteLine("CSV File Created");
         } // end of CSV-UserResponse
